f9b79f8324feb82f0fc05167ac1bcf392f9bc47a
muhammed fatih sarac <143208623+muhammedfatihsarac2@users.noreply.github.com>
2023-12-26 05:58:32 +0300

    Delete src directory

--- src/file/baseretrieve.js
+++
@@ -1,8 +0,0 @@
-module.exports = async(req, res) => {
-    res.status = 400
-    return res.json({
-        Status: 400,
-        Message: "Please provide a parameter",
-        Path: "/:id/:filename"
-    })
-}
